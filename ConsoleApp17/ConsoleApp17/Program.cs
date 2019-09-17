f3e61aa543e5f88c115ef6b820a9f20e15e9a348
Jingda Wang <Jingda_Wang@epam.com>
2019-09-17 12:15:15 +0800

    666

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA s = new AA();
+            List<AA> l = new List<AA>() { s };
+            var look = l.ToLookup(a => a.key);
+           
+            Console.Out.WriteLine(look[0].Count());
+            Console.Out.WriteLine(DateTime.Today.ToString(CultureInfo.InvariantCulture));
+            Console.Read();
+        }
+    }
+    class AA
+    {
+        public int key = 88;
+    }
+}
